dfcdc6cc265e16b825eb81112044755839ad9e6e
Vyoma-M <vyoma1993@gmail.com>
2026-02-17 14:45:46 +0100

    Modifications to `gettod.py`.

--- tau-A/gettod.py
+++ tau-A/gettod.py
@@ -1,59 +1,119 @@
+""" Module for loading Planck Time-Ordered Data (TOD) for tau-A analysis.
+Provides the `Get_TOD` class which can be configured with various parameters.
+One can then call methods like `tod()` or `tod_withcc()` to load the TOD data as a 
+`TOD` object containing signal, pixel indices (or x-y coordinates for TOD projected
+onto a grid), and weights. The MapMaker class can then use this TOD object to perform mapmaking.
+"""
 import os
+from dataclasses import dataclass, field
+from typing import Optional, Sequence, Dict
+
 import npipe_utils as utils
 import numpy as np
-from astropy.io import fits
+from astropy.io import fits, ascii
 import astropy.units as u
 import healpy as hp
+import logging
+
+
+@dataclass
+class GetTODConfig:
+    """
+    Configuration class for `Get_TOD` loader.
+    Parameters:
+    ------------
+    instrument: str
+        Planck instrument to load data for. Must be one of "HFI" or "LFI". Default is "HFI".
+    data_path: Optional[str]
+        Path to folder containing destriped Planck TOD files. If not provided, will use default path from `utils.get_data_path()`.
+    alpha: float
+        Spectral index for colour correction. Default is -0.28, appropriate for tau-A.
+    freq: int
+        Frequency channel to load data for. Default is 100 GHz.
+    coord: Optional[Sequence[float]]
+        Coordinates of the target field. If not provided, will default to tau-A coordinates based on `coord_system`.
+    coord_system: str
+        Coordinate system for `coord`. Must be one of "galactic" or "equatorial". Default is "galactic".
+    withcc: bool
+        Whether to apply colour correction to the TOD. Default is False.
+    f_bg: Optional[np.ndarray]
+        Background flux values for background subtraction, in K_CMB units. If `bg_subtraction` is True and this is not provided, default values will be used based on the instrument and frequencies.
+    bg_subtraction: bool
+        Whether to perform background subtraction using `f_bg`. Default is False.
+    nside: Optional[int]
+        Nside parameter for HEALPix pixelization.
+    planck_freq: Dict[str, Sequence[int]]
+        Dictionary mapping instruments to their available frequency channels. Default is {"LFI": [30, 40, 70], "HFI": [100, 143, 217, 353, 545, 857]}.
+    
+    Returns
+    -----------
+    GetTODConfig
+        An instance of the `GetTODConfig` dataclass with the specified configuration.
+    """
+    instrument: str = "HFI"
+    data_path: Optional[str] = None
+    alpha: float = -0.28
+    freq: int = 100
+    coord: Optional[Sequence[float]] = None
+    coord_system: str = "galactic"
+    withcc: bool = False
+    f_bg: Optional[np.ndarray] = None
+    bg_subtraction: bool = False
+    nside: Optional[int] = None
+    planck_freq: Dict[str, Sequence[int]] = field(
+        default_factory=lambda: {
+            "LFI": [30, 40, 70],
+            "HFI": [100, 143, 217, 353, 545, 857],
+        }
+    )
+
+    def __post_init__(self):
+        if self.coord is None:
+            if self.coord_system == "galactic":
+                self.coord = [184.5574, -5.7843]
+            else:
+                self.coord = [83.63304, 22.01449]
+        if self.bg_subtraction and self.f_bg is None:
+            self.f_bg = self._default_bg()
+
+    def _default_bg(self) -> np.ndarray:
+        if self.instrument == "LFI":
+            return np.array([1.70065975e-03, 1.54363888e-03, 2.03856413e-04])
+        return np.array([9.50687754e-5, 1.89320788e-4, 6.29073416e-4, 6.16040430e-3])
+
+
+@dataclass
+class TOD:
+    signal: np.ndarray
+    pixels: np.ndarray
+    weights: np.ndarray
+    theta: Optional[np.ndarray] = None
+    phi: Optional[np.ndarray] = None
+    nside: Optional[int] = None
 
 
 class Get_TOD:
-    def __init__(
-        self,
-        instrument="HFI",
-        data_path=None,
-        alpha=-0.28,
-        coord=None,
-        coord_system="galactic",
-        withcc=False,
-        f_bg=None,
-        bg_subtraction=False,
-    ):
-        self.data_path = data_path
-        self.alpha = alpha
-        self.instrument = instrument
-        self.coord = coord
-        self.coord_system = coord_system
-        self.withcc = withcc
-        self.f_bg = f_bg
-        self.bg_subtraction = bg_subtraction
-        self.planck_freq = {"LFI": [30, 40, 70], "HFI": [100, 143, 217, 353, 545, 857]}
-        if self.data_path is None:
-            self.data_path = utils.get_data_path(subfolder="data/")
-        if coord is None and coord_system == "galactic":
-            coord = [184.5574, -5.7843]  # galactic coord of tau-A
-        elif coord is None and coord_system == "equatorial":
-            coord = [83.63304, 22.01449]  # equatorial coord of tau-A
-        if self.bg_subtraction and self.f_bg is None:
-            if instrument == "LFI":
-                f_bg = np.array(
-                    [1.70065975e-03, 1.54363888e-03, 2.03856413e-04]
-                )  # in K_CMB units
-            elif instrument == "HFI":
-                f_bg = np.array(
-                    [9.50687754e-5, 1.89320788e-4, 6.29073416e-4, 6.16040430e-3]
-                )  # in K_CMB units
-            print(
-                "Background flux for background subtraction not provided. Using default values of"
-                "{} in K_CMB units for frequencies {}".format(
-                    f_bg, self.planck_freq[instrument]
-                )
-            )
-        if withcc:
-            print(
-                "Colour correction for SED with index {} will be applied".format(
-                    self.alpha
-                )
-            )
+    def __init__(self, config: Optional[GetTODConfig] = None, **kwargs):
+        if config is None:
+            config = GetTODConfig(**kwargs)
+        else:
+            for k, v in kwargs.items():
+                if hasattr(config, k):
+                    setattr(config, k, v)
+
+        self.config = config
+        self.data_path = config.data_path or utils.get_data_path(subfolder="data/")
+        self.alpha = config.alpha
+        self.instrument = config.instrument
+        self.coord = config.coord
+        self.coord_system = config.coord_system
+        self.withcc = config.withcc
+        self.f_bg = config.f_bg
+        self.bg_subtraction = config.bg_subtraction
+        self.freq = config.freq
+        self.planck_freq = config.planck_freq
+
+        logging.getLogger(__name__).info("Data path set to: %s", self.data_path)
 
         # Validate data path
         self._validate_data_path()
@@ -61,8 +121,13 @@ class Get_TOD:
     def _validate_data_path(self):
         if os.path.isdir(self.data_path):
             print(
-                f"Using {self.data_path} as the folder containing\
-                 destriped Planck TOD files."
+                f"Using {self.data_path} as the folder containing \
+destriped Planck TOD files."
+            )
+        elif not os.path.isdir(self.data_path):
+            raise FileNotFoundError(
+                f"Data path {self.data_path} does not exist. Please \
+provide a valid path to the folder containing destriped Planck TOD files."
             )
         elif os.path.isfile(self.data_path):
             raise IsADirectoryError(
@@ -71,7 +136,20 @@ class Get_TOD:
                 f"Please provide the full path to the data folder."
             )
 
-    def _get_tod(self, dets, nside):
+    def _read_detector_arrays(self, det):
+        """Read TOD for a single detector FITS file and return flattened arrays."""
+        path = os.path.join(self.data_path, "M1", f"small_dataset_M1_{det}.fits")
+        with fits.open(path) as hdul:
+            data = hdul[1].data
+            theta = data["theta"].ravel()
+            phi = data["phi"].ravel()
+            iweight = np.ones_like(data["qweight"].ravel())
+            qweight = data["qweight"].ravel()
+            uweight = data["uweight"].ravel()
+            signal = data["signal"].ravel()
+        return theta, phi, iweight, qweight, uweight, signal
+
+    def _get_tod(self, freq, nside):
         pixels = []
         iweights = []
         qweights = []
@@ -79,21 +157,16 @@ class Get_TOD:
         signal = []
         thetas = []
         phis = []
-
+        dets = utils.get_dets(freq)
         for det in dets:
-            with fits.open(
-                self.data_path + "M1/small_dataset_M1_{}.fits".format(det)
-            ) as hdul:
-                data = hdul[1].data
-                theta = data["theta"].ravel()
-                thetas.append(theta)
-                phi = data["phi"].ravel()
-                phis.append(phi)
-                pixels.append(hp.ang2pix(nside, theta, phi, nest=True, lonlat=False))
-                iweights.append(np.ones_like(data["qweight"].ravel()))
-                qweights.append(data["qweight"].ravel())
-                uweights.append(data["uweight"].ravel())
-                signal.append(data["signal"].ravel())
+            theta, phi, iwt, qwt, uwt, sig = self._read_detector_arrays(det)
+            thetas.append(theta)
+            phis.append(phi)
+            pixels.append(hp.ang2pix(nside, theta, phi, nest=True, lonlat=False))
+            iweights.append(iwt)
+            qweights.append(qwt)
+            uweights.append(uwt)
+            signal.append(sig)
         signal = np.hstack(signal)
         pixels = np.hstack(pixels)
         pixweights = np.vstack(
@@ -105,7 +178,7 @@ class Get_TOD:
         )
         return signal, pixels, pixweights
 
-    def _get_tod_withcc(self, dets, nside, alpha, instrument, bg_subtraction):
+    def _get_tod_withcc(self, freq, nside, alpha, instrument, bg_subtraction):
         pixels = []
         iweights = []
         qweights = []
@@ -113,34 +186,43 @@ class Get_TOD:
         signal = []
         thetas = []
         phis = []
-        fr = self.planck_freq[self.instrument]
+        dets = utils.get_dets(freq)
         for det in dets:
-            with fits.open(
-                self.data_path + "M1/small_dataset_M1_{}.fits".format(det)
-            ) as hdul:
-                data_uccc = ascii.read(self.data_path + "/PR2-3/UC_CC_RIMO-4_alpha{}.txt".format(alpha))
-                uc_cc = np.array((data_uccc["UCxCC"]))
-                detnames = np.array((data_uccc["Detector-name"]))
+            # read detector arrays
+            theta, phi, iwt, qwt, uwt, sig = self._read_detector_arrays(det)
+            # read colour-correction table (try instrument-prefixed filename, fallback to unprefixed)
+            cc_fname = os.path.join(
+                self.data_path, "PR2-3", f"{instrument}_UC_CC_RIMO-4_alpha{alpha}.txt"
+            )
+            if not os.path.exists(cc_fname):
+                cc_fname = os.path.join(
+                    self.data_path, "PR2-3", f"UC_CC_RIMO-4_alpha{alpha}.txt"
+                )
+            data_uccc = ascii.read(cc_fname)
+            uc_cc = np.array((data_uccc["UCxCC"]))
+            detnames = np.array((data_uccc["Detector-name"]))
+            # find detector index in cc table
+            ind = np.where(detnames == det)[0]
+            if ind.size == 0:
+                # if not found, skip this detector
+                continue
+            thetas.append(theta)
+            phis.append(phi)
+            pixels.append(hp.ang2pix(nside, theta, phi, nest=True, lonlat=False))
+            iweights.append(iwt)
+            qweights.append(qwt)
+            uweights.append(uwt)
+            # background subtraction
+            if bg_subtraction and (self.f_bg is not None):
                 if instrument == "HFI":
-                    det_split = det.split("-")[0]
-                    f_ind = np.where(int(det_split) == fr)
-                ind = np.where(detnames == det)[0]
-                data = hdul[1].data
-                theta = data["theta"].ravel()
-                thetas.append(theta)
-                phi = data["phi"].ravel()
-                phis.append(phi)
-                pixels.append(hp.ang2pix(nside, theta, phi, nest=True, lonlat=False))
-                iweights.append(np.ones_like(data["qweight"].ravel()))
-                qweights.append(data["qweight"].ravel())
-                uweights.append(data["uweight"].ravel())
-                sig = np.array(data["signal"].ravel())
-                if bg_subtraction:
-                    if instrument == "HFI":
-                        sig -= self.f_bg[f_ind]
-                    elif instrument == "LFI":
-                        sig -= self.f_bg
-                signal.append(sig * uc_cc[ind])
+                    try:
+                        f_ind = list(self.planck_freq[instrument]).index(freq)
+                    except ValueError:
+                        f_ind = 0
+                    sig = sig - self.f_bg[f_ind]
+                elif instrument == "LFI":
+                    sig = sig - self.f_bg
+            signal.append(sig * uc_cc[ind])
         signal = np.hstack(signal)
         pixels = np.hstack(pixels)
         pixweights = np.vstack(
@@ -152,26 +234,22 @@ class Get_TOD:
         )
         return signal, pixels, pixweights
 
-    def _get_tod_ongrid(self, coord, dets, npix, pixsize, coord_system="galactic"):
+    def _get_tod_ongrid(self, coord, freq, npix, pixsize, coord_system="galactic"):
         iweights = []
         qweights = []
         uweights = []
         signal = []
         thetas = []
         phis = []
+        dets = utils.get_dets(freq)
         for det in dets:
-            with fits.open(
-                self.data_path + "M1/small_dataset_M1_{}.fits".format(det)
-            ) as hdul:
-                data = hdul[1].data
-                theta = data["theta"].ravel()
-                phi = data["phi"].ravel()
-                thetas.append(theta)
-                phis.append(phi)
-                iweights.append(np.ones_like(data["qweight"].ravel()))
-                qweights.append(data["qweight"].ravel())
-                uweights.append(data["uweight"].ravel())
-                signal.append(data["signal"].ravel())
+            theta, phi, iwt, qwt, uwt, sig = self._read_detector_arrays(det)
+            thetas.append(theta)
+            phis.append(phi)
+            iweights.append(iwt)
+            qweights.append(qwt)
+            uweights.append(uwt)
+            signal.append(sig)
 
         pixweights = np.vstack(
             [
@@ -199,3 +277,67 @@ class Get_TOD:
             )
             x, y, xbinning, ybinning = utils.pixelize_skycoords(ra, dec, w2, side=npix)
         return x, y, xbinning, ybinning, signal, pixweights
+
+    # Public wrappers
+    def tod(self, freq: Optional[int] = None, nside: Optional[int] = None) -> TOD:
+        """Load TOD for given frequency and nside and return a `TOD` object.
+
+        Defaults to configuration values when arguments are not provided.
+        """
+        freq = freq or self.freq
+        nside = nside or self.config.nside
+        if nside is None:
+            raise ValueError(
+                "nside must be provided either in config or as an argument"
+            )
+        signal, pixels, pixweights = self._get_tod(freq, nside)
+        return TOD(signal=signal, pixels=pixels, weights=pixweights, nside=nside)
+
+    def tod_withcc(
+        self,
+        freq: Optional[int] = None,
+        nside: Optional[int] = None,
+        alpha: Optional[float] = None,
+        instrument: Optional[str] = None,
+        bg_subtraction: Optional[bool] = None,
+    ) -> TOD:
+        """Load TOD with colour correction applied and return a `TOD` object.
+
+        Arguments override configuration when provided.
+        """
+        freq = freq or self.freq
+        nside = nside or self.config.nside
+        if nside is None:
+            raise ValueError(
+                "nside must be provided either in config or as an argument"
+            )
+        alpha = alpha if alpha is not None else self.alpha
+        instrument = instrument or self.instrument
+        bg_subtraction = (
+            bg_subtraction if bg_subtraction is not None else self.bg_subtraction
+        )
+        signal, pixels, pixweights = self._get_tod_withcc(
+            freq, nside, alpha, instrument, bg_subtraction
+        )
+        return TOD(signal=signal, pixels=pixels, weights=pixweights, nside=nside)
+
+    def tod_ongrid(
+        self,
+        coord: Optional[Sequence[float]] = None,
+        freq: Optional[int] = None,
+        npix: int = 80,
+        pixsize: float = 1.5,
+        coord_system: Optional[str] = None,
+    ):
+        """Project TOD onto a square grid around `coord`.
+        Only use this for small fields where flat projection
+        is applicable.
+
+        Returns: x, y, xbinning, ybinning, signal, pixweights
+        """
+        coord = coord or self.coord
+        freq = freq or self.freq
+        coord_system = coord_system or self.coord_system
+        return self._get_tod_ongrid(
+            coord, freq, npix, pixsize, coord_system=coord_system
+        )
